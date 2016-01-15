13503c8a01a3023c7aaee877049bbcdad6d8593e
ireineck <ireineck@student.cvtc.edu>
2016-01-14 20:39:16 -0600

    makes catagroy a required field

--- savvyChef/app/models/special.rb
+++ savvyChef/app/models/special.rb
@@ -1,5 +1,5 @@
 class Special < ActiveRecord::Base
     belongs_to :category
-    validates_presence_of :title, :ingredients, :instructions
+    validates_presence_of :title, :ingredients, :instructions,:category
     validates :title, uniqueness: true
 end
