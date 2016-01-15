4dc73494e51117bd5dbe699f9ff3ca62f04551c9
ireineck <ireineck@student.cvtc.edu>
2016-01-14 21:08:43 -0600

    adds price field to the specials table

--- savvyChef/db/schema.rb
+++ savvyChef/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110034708) do
+ActiveRecord::Schema.define(version: 20160115030743) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160110034708) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "category_id"
+    t.decimal  "price"
   end
 
   add_index "specials", ["category_id"], name: "index_specials_on_category_id"
