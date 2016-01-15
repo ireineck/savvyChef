b47fd0cf3b65a04553d07c478c47f92c46ec1e00
ireineck <ireineck@student.cvtc.edu>
2016-01-14 21:39:57 -0600

    adds :numberSold to special_params in special_controller, adds fields for adding number sold to views/specials/_form.html.erb, prints number sold in views/specials/show.html.erb

--- savvyChef/app/controllers/specials_controller.rb
+++ savvyChef/app/controllers/specials_controller.rb
@@ -71,7 +71,7 @@ class SpecialsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def special_params
-        params.require(:special).permit(:title, :category_id, :ingredients, :instructions, :price)
+        params.require(:special).permit(:title, :category_id, :ingredients, :instructions, :price, :numberSold)
     end
     
     def get_categories
