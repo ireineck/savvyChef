41412ed6ab5aac9acf69b856255d819f50d1da37
ireineck <ireineck@student.cvtc.edu>
2016-01-20 12:03:58 -0600

    fixes indentation in category_controller.rb to match ruby convention

--- savvyChef/app/controllers/category_controller.rb
+++ savvyChef/app/controllers/category_controller.rb
@@ -1,10 +1,12 @@
 class CategoryController < ApplicationController
 
-def index
-#    @specials = Special.all
-#    @specials = @category.specials
+  def index
+    #    @specials = Special.all
+    #    @specials = @category.specials
     @categorys = Category.all
-end
+  end
+
+  
 
 
 
