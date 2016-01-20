2cb6b69cbb0cb6cfea0e46d8180eb957891bd383
ireineck <ireineck@student.cvtc.edu>
2016-01-20 11:47:40 -0600

    adds very basic list of categoies to category#index

--- savvyChef/app/controllers/category_controller.rb
+++ savvyChef/app/controllers/category_controller.rb
@@ -1,2 +1,16 @@
 class CategoryController < ApplicationController
+
+def index
+#    @specials = Special.all
+#    @specials = @category.specials
+    @categorys = Category.all
+end
+
+
+
+
+
+
+
+
 end
