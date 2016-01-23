b27d871c512c3e99c6fab8374ffc210af8b7eddd
ireineck <ireineck@student.cvtc.edu>
2016-01-23 15:36:19 -0600

    adds authenticate method to application_controller.rb.. This method utilizes the basic http authenticationprovided by rails for simple authentication.

--- savvyChef/app/controllers/application_controller.rb
+++ savvyChef/app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  
+  private
+  # HTTP Basic Authentication
+  # This is not very secure, will later switch to devise
+  def authenticate
+    authenticate_or_request_with_http_basic do  |user_name, password|
+      session[:logged_in] = (user_name == 'admin' && password == 'password')
+    end
+  end
+  
 end
